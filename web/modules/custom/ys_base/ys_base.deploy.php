8c6f8a9db594490ed5494dad60e79a31eae4407a
Alex Skrypnyk <alex@drevops.com>
2025-05-29 21:44:19 +1000

    Renamed ys_core module to ys_base - updated directory and file names.

--- web/modules/custom/ys_base/ys_base.deploy.php
+++ web/modules/custom/ys_base/ys_base.deploy.php
@@ -16,7 +16,7 @@ declare(strict_types=1);
  *
  * @codeCoverageIgnore
  */
-function ys_core_deploy_install_theme(): void {
+function ys_base_deploy_install_theme(): void {
   \Drupal::service('theme_installer')->install(['olivero']);
   \Drupal::service('theme_installer')->install(['your_site_theme']);
   \Drupal::service('config.factory')->getEditable('system.theme')->set('default', 'your_site_theme')->save();
